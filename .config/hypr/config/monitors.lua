dfb4275e7108b6f8a146e553143b09ac26137758
vallfee <vallfee.ai@gmail.com>
2026-07-07 20:35:25 +0300

    change rofi theme

--- .config/hypr/config/monitors.lua
+++ .config/hypr/config/monitors.lua
@@ -4,7 +4,7 @@ hl.monitor({
     output    = "eDP-1",
     mode      = "1920x1080@60",
     position  = "0x0",
-    scale     = "auto",
+    scale     = "auto`",
 })
 hl.monitor({
     output    = "HDMI-A-1",
