18e7cc7f17d6b0731ff324ab3723129bbe8611fa
vallfee <vallfee.ai@gmail.com>
2026-07-21 16:37:03 +0300

    add install script and zapret

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo mv ~/dotfiles/.config ~/
+sudo mv ~/dotfiles/.themes ~/
+paru vscodium
+sudo pacman -S waybar
+paru waybar-cava
+paru cava 
+paru karing
+sudo pacman -S ffmpeg
+sudo pacman -S yt-dlp
+sudo pacman -S discord
